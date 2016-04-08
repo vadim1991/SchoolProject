82f0d9119afb20ccaacb983f314ad51ba2293a05
vadim1991 <vilana23>
2016-04-08 05:57:39 -0500

    fix footer

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-    $("#" + "${param.page}").addClass("active");
-});
-
 $("#add-ingr").click(function () {
     addOneMore(".ingredient", "#ingredients", "Ingredient ");
 });
